06c67a7df4e2fd5cbc221f2a9c3f64179af91344
sshwsfc <sshwsfc@gmail.com>
2012-11-19 18:42:24 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-exadmin',
+    version='0.1.0',
+    description='New style and free plugin django admin module, UI base bootstrap2.',
+    author='TM (sshwsfc)',
+    author_email='sshwsfc@gmail.com',
+    url='http://github.com/sshwsfc/django-exadmin',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
