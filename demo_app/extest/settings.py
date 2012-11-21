6473ec63e4cf32827f681aee7fcda99bc02c045e
sshwsfc <sshwsfc@gmail.com>
2012-11-21 14:48:28 +0800

    change database data to english

--- demo_app/extest/settings.py
+++ demo_app/extest/settings.py
@@ -36,11 +36,11 @@ except Exception:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
