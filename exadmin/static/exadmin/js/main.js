a17a412b1b8a3a06c5d7e36545c3165ab1161621
sshwsfc <sshwsfc@gmail.com>
2012-11-28 17:05:54 +0800

    create exform method

--- exadmin/static/exadmin/js/main.js
+++ exadmin/static/exadmin/js/main.js
@@ -1,3 +1,13 @@
 ;(function($){
   $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+
+  $.fn.exform = function(){
+    this.each(function () {
+      for (var i = $.fn.exform.renders.length - 1; i >= 0; i--) {
+        $.fn.exform.renders[i]($(this))
+      };
+    })
+  }
+  $.fn.exform.renders = [];
+  
 })(jQuery)
