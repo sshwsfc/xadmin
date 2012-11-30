05c924fa9d8211148a86895ef3a2c3c2d8af7b02
差沙 <tm@wiyun.net>
2012-12-01 00:27:40 +0800

    do portal in portal.js

--- exadmin/static/exadmin/js/form.js
+++ exadmin/static/exadmin/js/form.js
@@ -31,27 +31,6 @@
       });
 
     $(function() {
-        $( ".form-column" ).sortable({
-            connectWith: ".form-column",
-            handle: '.box-title',
-            forcePlaceholderSize: true,
-            cursor: "move",
-            cancel: ".unsort"
-        });
-
-        $( ".box" )
-            .find( ".box-title" )
-                .prepend( "<i class='icon icon-chevron-up chevron'></i>")
-                .end()
-            .find( ".box-content" );
-
-        $( ".box-title .icon.chevron" ).click(function() {
-            $( this ).toggleClass( "icon-chevron-up" ).toggleClass( "icon-chevron-down" );
-            $( this ).parents( ".box:first" ).find( ".box-content" ).toggle('fast');
-        });
-
-        $( ".form-column" ).disableSelection();
-
         $('.exform').exform();
     });
 
--- exadmin/static/exadmin/js/portal.js
+++ exadmin/static/exadmin/js/portal.js
@@ -17,4 +17,6 @@ jQuery(function() {
         $( this ).toggleClass( "icon-chevron-up" ).toggleClass( "icon-chevron-down" );
         $( this ).parents( ".box:first" ).find( ".box-content" ).toggle('fast');
     });
+    
+    $( ".column" ).disableSelection();
 });
