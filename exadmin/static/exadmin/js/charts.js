25a9653b97dbdc34fc4734abf49a9ccc30906351
sshwsfc <sshwsfc@gmail.com>
2012-11-30 18:47:16 +0800

    auto render chart by class .init and use class instead of id.

--- exadmin/static/exadmin/js/charts.js
+++ exadmin/static/exadmin/js/charts.js
@@ -9,39 +9,46 @@ $(document).ready(function(){
         }).appendTo("body").fadeIn(200);
     }
 
-    $('#chart-tab a').click(function(e){
+    $.fn.chart = function(){
+      $(this).each(function(){
+        var $chart = $(this);
+
+        if($chart.data('chart-obj')) return;
+
+        $chart.html('<span class="muted"><i class="icon icon-refresh"></i> Loading chart...</span>');
+
+        $.getJSON($chart.data('chart-url'), function(data){
+            var chart = $.plot($chart, data.data, data.option);
+            var previousPoint = null;
+            $chart.bind("plothover", function (event, pos, item) {
+                if (item) {
+                    if (previousPoint != item.dataIndex) {
+                        previousPoint = item.dataIndex;
+                        
+                        $("#chart-tooltip").remove();
+                        var x = item.series.xaxis.tickFormatter(item.datapoint[0], item.series.xaxis),
+                            y = item.series.yaxis.tickFormatter(item.datapoint[1], item.series.yaxis);
+                        console.log(item);
+                        
+                        showTooltip(item.pageX, item.pageY,
+                                    item.series.label + " : " + x + " , " + y);
+                    }
+                } else {
+                    $("#chart-tooltip").remove();
+                    previousPoint = null;            
+                }
+            });
+            $chart.data('chart-obj', chart);
+        });
+      })
+    }
+
+    $('.chart-tab a').click(function(e){
       e.preventDefault();
       $(this).tab('show');
 
-      var $chart = $($(this).attr('href'));
-
-      if($chart.data('chart-obj')) return;
-
-      $chart.html('<span class="muted"><i class="icon icon-refresh"></i> Loading chart...</span>');
-
-      $.getJSON($chart.data('chart-url'), function(data){
-          var chart = $.plot($chart, data.data, data.option);
-          var previousPoint = null;
-          $chart.bind("plothover", function (event, pos, item) {
-              if (item) {
-                  if (previousPoint != item.dataIndex) {
-                      previousPoint = item.dataIndex;
-                      
-                      $("#chart-tooltip").remove();
-                      var x = item.series.xaxis.tickFormatter(item.datapoint[0], item.series.xaxis),
-                          y = item.series.yaxis.tickFormatter(item.datapoint[1], item.series.yaxis);
-                      console.log(item);
-                      
-                      showTooltip(item.pageX, item.pageY,
-                                  item.series.label + " : " + x + " , " + y);
-                  }
-              } else {
-                  $("#chart-tooltip").remove();
-                  previousPoint = null;            
-              }
-          });
-          $chart.data('chart-obj', chart);
-      });
+      $($(this).attr('href')).chart();
     });
-    $('#chart-tab a:first').click();
+    $('.chart-tab a:first').click();
+    $('.chart.init').chart();
 });
