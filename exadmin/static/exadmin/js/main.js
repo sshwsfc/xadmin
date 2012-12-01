4a8e272c6574af353de71ef2dc5d3529864bf9fe
差沙 <tm@wiyun.net>
2012-12-02 00:10:41 +0800

    move getCookie, add save_user_settings func.

--- exadmin/static/exadmin/js/main.js
+++ exadmin/static/exadmin/js/main.js
@@ -10,9 +10,42 @@
   }
   $.fn.exform.renders = [];
 
+  $.getCookie = function(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
+
   // g-search
   $('#g-search .dropdown-menu a').click(function(){
       $('#g-search form').attr('action', $(this).data('action')).submit();
   })
+
+  // save settings
+  $.save_user_settings = function(key, value, success, error){
+    var csrftoken = $.getCookie('csrftoken');
+    $.ajax({
+      type: 'POST',
+      url: '/settings/user',
+      data: {'key': key, 'value': value},
+      success: success,
+      error: error,
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      beforeSend: function(xhr, settings) {
+          xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    });
+  }
   
 })(jQuery)
