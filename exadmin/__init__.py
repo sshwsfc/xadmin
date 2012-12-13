6f90db30ec0622f2896d9473f493f07056c3381a
sshwsfc <sshwsfc@gmail.com>
2012-12-13 16:54:57 +0800

    rename admin.py to xadmin.py for compact with django-admin file

--- exadmin/__init__.py
+++ exadmin/__init__.py
@@ -16,7 +16,7 @@ def autodiscover():
         # Attempt to import the app's admin module.
         try:
             before_import_registry = site.copy_registry()
-            import_module('%s.admin' % app)
+            import_module('%s.xadmin' % app)
         except:
             # Reset the model registry to the state before the last import as
             # this import will have to reoccur on the next request and this
@@ -27,7 +27,7 @@ def autodiscover():
             # Decide whether to bubble up this error. If the app just
             # doesn't have an admin module, we can ignore the error
             # attempting to import it, otherwise we want it to bubble up.
-            if module_has_submodule(mod, 'admin'):
+            if module_has_submodule(mod, 'xadmin'):
                 raise
 
     from exadmin import views
