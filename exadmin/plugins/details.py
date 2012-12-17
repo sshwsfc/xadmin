343992520dbd43f5fe02d2c89796606ccf06a343
差沙 <tm@wiyun.net>
2012-12-18 00:06:16 +0800

    change detail res-uri to detail view url

--- exadmin/plugins/details.py
+++ exadmin/plugins/details.py
@@ -5,27 +5,22 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 from exadmin.sites import site
-from exadmin.views import BaseAdminPlugin, ListAdminView, api_manager
+from exadmin.views import BaseAdminPlugin, ListAdminView
 
 class DetailsPlugin(BaseAdminPlugin):
 
     show_detail_fields = []
     show_all_rel_details = True
 
-    def get_details_url(self, obj):
-        resuorce = api_manager.get_resource(obj)
-        if resuorce:
-            return resuorce.get_resource_uri(obj)
-        return None
-
     def result_item(self, item, obj, field_name, row):
         if hasattr(item.field, 'rel') and isinstance(item.field.rel, models.ManyToOneRel) \
             and (self.show_all_rel_details or (field_name in self.show_detail_fields)):
             rel_obj = getattr(obj, field_name)
             if rel_obj:
-                item_res_uri = self.get_details_url(rel_obj)
+                opts = rel_obj._meta
+                item_res_uri = reverse('%s:%s_%s_detail' % (self.admin_site.app_name, opts.app_label, opts.module_name), \
+                        args=(getattr(rel_obj, opts.pk.attname),))
                 if item_res_uri:
-                    opts = rel_obj._meta
                     edit_url = reverse('%s:%s_%s_change' % (self.admin_site.app_name, opts.app_label, opts.module_name), \
                         args=(getattr(rel_obj, opts.pk.attname),))
                     item.btns.append('<a data-res-uri="%s" data-edit-uri="%s" class="details-handler" rel="tooltip" title="%s"><i class="icon-info-sign"></i></a>' \
