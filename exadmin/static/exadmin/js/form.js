5412437fae7743eb37d23dc25e19b12d47a136ae
sshwsfc <sshwsfc@gmail.com>
2012-12-17 13:05:10 +0800

    fixed action bar

--- exadmin/static/exadmin/js/form.js
+++ exadmin/static/exadmin/js/form.js
@@ -29,9 +29,17 @@
             });
         })
       });
-
     $(function() {
         $('.exform').exform();
+        var action_bar = $('.form-actions');
+        var height=action_bar[0].offsetTop + action_bar.outerHeight();
+        var onchange = function(){
+            var s=(document.body.scrollTop||document.documentElement.scrollTop) + window.innerHeight;
+            if(s<height){action_bar.addClass('fixed');}
+            else{action_bar.removeClass('fixed');}
+        }
+        window.onscroll=onchange;
+        onchange();
     });
 
 })(jQuery)
