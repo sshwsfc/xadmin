5e433a68740a7bd6c6139ea980db337c4abc6921
sshwsfc <sshwsfc@gmail.com>
2012-12-17 13:52:24 +0800

    do not make fixed action bar when no action bar in the page.

--- exadmin/static/exadmin/js/form.js
+++ exadmin/static/exadmin/js/form.js
@@ -32,14 +32,16 @@
     $(function() {
         $('.exform').exform();
         var action_bar = $('.form-actions');
-        var height=action_bar[0].offsetTop + action_bar.outerHeight();
-        var onchange = function(){
-            var s=(document.body.scrollTop||document.documentElement.scrollTop) + window.innerHeight;
-            if(s<height){action_bar.addClass('fixed');}
-            else{action_bar.removeClass('fixed');}
+        if(action_bar.length){
+            var height=action_bar[0].offsetTop + action_bar.outerHeight();
+            var onchange = function(){
+                var s=(document.body.scrollTop||document.documentElement.scrollTop) + window.innerHeight;
+                if(s<height){action_bar.addClass('fixed');}
+                else{action_bar.removeClass('fixed');}
+            }
+            window.onscroll=onchange;
+            onchange();
         }
-        window.onscroll=onchange;
-        onchange();
     });
 
 })(jQuery)
