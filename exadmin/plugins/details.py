bca2f04adb1c1e20d7b82480380b44f1524328c9
sshwsfc <sshwsfc@gmail.com>
2012-12-17 13:50:43 +0800

    do not show data details when rel_obj is None.

--- exadmin/plugins/details.py
+++ exadmin/plugins/details.py
@@ -22,13 +22,14 @@ class DetailsPlugin(BaseAdminPlugin):
         if hasattr(item.field, 'rel') and isinstance(item.field.rel, models.ManyToOneRel) \
             and (self.show_all_rel_details or (field_name in self.show_detail_fields)):
             rel_obj = getattr(obj, field_name)
-            item_res_uri = self.get_details_url(rel_obj)
-            if item_res_uri:
-                opts = rel_obj._meta
-                edit_url = reverse('%s:%s_%s_change' % (self.admin_site.app_name, opts.app_label, opts.module_name), \
-                    args=(getattr(rel_obj, opts.pk.attname),))
-                item.btns.append('<a data-res-uri="%s" data-edit-uri="%s" class="details-handler" rel="tooltip" title="%s"><i class="icon-info-sign"></i></a>' \
-                    % (item_res_uri, edit_url, _(u'Details of %s' % str(rel_obj))))
+            if rel_obj:
+                item_res_uri = self.get_details_url(rel_obj)
+                if item_res_uri:
+                    opts = rel_obj._meta
+                    edit_url = reverse('%s:%s_%s_change' % (self.admin_site.app_name, opts.app_label, opts.module_name), \
+                        args=(getattr(rel_obj, opts.pk.attname),))
+                    item.btns.append('<a data-res-uri="%s" data-edit-uri="%s" class="details-handler" rel="tooltip" title="%s"><i class="icon-info-sign"></i></a>' \
+                        % (item_res_uri, edit_url, _(u'Details of %s' % str(rel_obj))))
         return item
 
     # Media
