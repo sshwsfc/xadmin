fe858d52acd05f7f65510746a1ba52e3a51c10a6
差沙 <tm@wiyun.net>
2012-12-20 21:55:34 +0800

    load reversion models.

--- demo_app/extest/urls.py
+++ demo_app/extest/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 import exadmin
 exadmin.autodiscover()
 
+from exadmin.plugins import xversion
+xversion.registe_models()
+
 urlpatterns = patterns('',
     url(r'', include(exadmin.site.urls)),
 )
