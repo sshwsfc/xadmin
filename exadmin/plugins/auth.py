fafb81587c4505ec65881dc9e649ccf6be43b8ba
sshwsfc <sshwsfc@gmail.com>
2013-01-06 19:01:34 +0800

    improve user form.

--- exadmin/plugins/auth.py
+++ exadmin/plugins/auth.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User, Group
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import (UserCreationForm, UserChangeForm,
+    AdminPasswordChangeForm)
 from django.views.decorators.csrf import csrf_protect
 from exadmin.sites import site
+from exadmin.layout import *
 
 csrf_protect_m = method_decorator(csrf_protect)
 
@@ -11,26 +14,46 @@ class GroupAdmin(object):
     ordering = ('name',)
 
 class UserAdmin(object):
-    #add_form_template = 'admin/auth/user/add_form.html'
     change_user_password_template = None
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2')}
-        ),
-    )
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
 
+    def get_model_form(self, **kwargs):
+        if self.org_obj is None:
+            self.form = UserCreationForm
+        else:
+            self.form = UserChangeForm
+        return super(UserAdmin, self).get_model_form(**kwargs)
+
+    def get_form_layout(self):
+        if self.org_obj:
+            self.form_layout = (
+                Main(
+                    Fieldset('',
+                        'username', 'password',
+                        css_class='unsort no_title'
+                    ),
+                    Fieldset(_('Personal info'),
+                        Row('first_name', 'last_name'),
+                        Row(AppendedText('hard_disk', 'G'), AppendedText('memory', "G")),
+                        'email'
+                    ),
+                    Fieldset(_('Permissions'),
+                        'groups', 'user_permissions'
+                    ),
+                    Fieldset(_('Important dates'),
+                        'last_login', 'date_joined'
+                    ),
+                ),
+                Side(
+                    Fieldset(_('Status'),
+                        'is_active', 'is_staff', 'is_superuser',
+                    ),
+                )
+            )
+        return super(UserAdmin, self).get_form_layout()
 
 site.register(Group, GroupAdmin)
 site.register(User, UserAdmin)
