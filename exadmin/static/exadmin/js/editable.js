721efe9b2ddd4f92a1c2aea8dbdfa40909ecb3b4
sshwsfc <sshwsfc@gmail.com>
2013-01-09 16:47:38 +0800

    popover placement will find enough space to place.

--- exadmin/static/exadmin/js/editable.js
+++ exadmin/static/exadmin/js/editable.js
@@ -233,9 +233,87 @@
     constructor: Editable
 
     , einit: function( type, element, options ) {
-      this.$form.submit($.proxy(this.submit, this));
-
       this.cinit(type, element, options);
+
+      this.$form.submit($.proxy(this.submit, this));
+    }
+    , getPlacement: function(){
+      var $tip = this.tip();
+      var $el = this.$element;
+
+      var tip_width = $tip.width(),
+          tip_height = $tip.height(),
+          el_width = $el.width(),
+          el_height = $el.height(),
+          client_width = document.body.clientWidth,
+          gap = 20;
+
+      var top_gap = $el.offset().top - $("body").scrollTop() - 40,
+          left_gap = $el.offset().left - $("body").scrollLeft(),
+          right_gap = client_width - left_gap - el_width;
+
+      if(top_gap > tip_height + gap && left_gap > tip_width/2 + gap && right_gap > tip_width/2 + gap){
+        return 'top';
+      }
+      if(top_gap > tip_height/2){
+        if(right_gap > tip_width + gap){
+          return 'right';
+        } else if(left_gap > tip_width + gap) {
+          return 'left';
+        }
+      }
+      return 'bottom';
+    }  
+    , show: function () {
+      var $tip
+        , inside
+        , pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , tp
+
+      if (this.hasContent() && this.enabled) {
+        $tip = this.tip()
+        this.setContent()
+
+        if (this.options.animation) {
+          $tip.addClass('fade')
+        }
+
+        $tip
+          .detach()
+          .css({ top: 0, left: 0, display: 'block' })
+          .insertAfter(this.$element)
+
+        placement = this.getPlacement();
+
+        inside = /in/.test(placement)
+        pos = this.getPosition(inside)
+
+        actualWidth = $tip[0].offsetWidth
+        actualHeight = $tip[0].offsetHeight
+
+        switch (inside ? placement.split(' ')[1] : placement) {
+          case 'bottom':
+            tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'top':
+            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'left':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
+            break
+          case 'right':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}
+            break
+        }
+
+        $tip
+          .offset(tp)
+          .addClass(placement)
+          .addClass('in')
+      }
     }
     , setContent: function () {
       if(!this.rendered_form){
