a08053b401b1124abfb0d6e0252b0d3ad17cc9a8
sshwsfc <sshwsfc@gmail.com>
2013-01-21 12:03:07 +0800

    not add quick add btn on inline models.

--- exadmin/plugins/quickform.py
+++ exadmin/plugins/quickform.py
@@ -90,7 +90,7 @@ class QuickAddBtnPlugin(BaseAdminPlugin):
     #     return not self.request.is_ajax()
 
     def formfield_for_dbfield(self, formfield, db_field, **kwargs):
-        if isinstance(db_field, (models.ForeignKey, models.ManyToManyField)):
+        if self.model in self.admin_site._registry and isinstance(db_field, (models.ForeignKey, models.ManyToManyField)):
             rel_model = get_model_from_relation(db_field)
             if rel_model in self.admin_site._registry and self.has_model_perm(rel_model, 'add'):
                 add_url = self.get_model_url(rel_model, 'add')
