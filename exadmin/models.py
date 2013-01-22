71b5d8d2fe89a47c1194b7d8ef1399df5fed7767
sshwsfc <sshwsfc@gmail.com>
2013-01-22 23:35:14 +0800

    add sync_db post hook for add models view permission

--- exadmin/models.py
+++ exadmin/models.py
@@ -8,7 +8,32 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.base import ModelBase
 from django.utils.encoding import smart_unicode
 
+from django.db.models.signals import post_syncdb
+from django.contrib.auth.models import Permission
+ 
 import datetime, decimal
+
+def add_view_permissions(sender, **kwargs):
+    """
+    This syncdb hooks takes care of adding a view permission too all our 
+    content types.
+    """
+    # for each of our content types
+    for content_type in ContentType.objects.all():
+        # build our permission slug
+        codename = "view_%s" % content_type.model
+ 
+        # if it doesn't exist..
+        if not Permission.objects.filter(content_type=content_type, codename=codename):
+            # add it
+            Permission.objects.create(content_type=content_type,
+                                      codename=codename,
+                                      name="Can view %s" % content_type.name)
+            print "Added view permission for %s" % content_type.name
+ 
+# check for all our view permissions after a syncdb
+post_syncdb.connect(add_view_permissions)
+
 
 class Bookmark(models.Model):
     title = models.CharField(_(u'Title'), max_length=128)
