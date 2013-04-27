80dcbdf83f731f8f3c81f15a213ae65fb812b9c5
sshwsfc <sshwsfc@gmail.com>
2013-04-27 22:58:42 +0800

    Setup vendors xstatic conf.

--- xadmin/staticx.py
+++ xadmin/staticx.py
@@ -16,6 +16,57 @@ statics = {
         "js": {
             'dev': 'xadmin/vendor/jquery/jquery.js',
             'production': 'xadmin/vendor/jquery/jquery.min.js',
+        },
+        "ui": {
+            "js": {"dev": "xadmin/js/jquery-ui.js"}
         }
-    }
+    },
+    "font-awesome": {
+        "css": {
+            'dev': 'xadmin/vendor/font-awesome/css/font-awesome.css',
+            'production': 'xadmin/vendor/font-awesome/css/font-awesome.min.css',
+        }
+    },
+    "timepicker": {
+        "css": {
+            'dev': 'xadmin/vendor/bootstrap-timepicker/css/bootstrap-timepicker.css',
+            'production': 'xadmin/vendor/bootstrap-timepicker/css/bootstrap-timepicker.min.css',
+        },
+        "js": {
+            'dev': 'xadmin/vendor/bootstrap-timepicker/js/bootstrap-timepicker.js',
+            'production': 'xadmin/vendor/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
+        }
+    },
+    "datepicker": {
+        "css": {
+            'dev': 'xadmin/vendor/bootstrap-datepicker/css/datepicker.css'
+        },
+        "js": {
+            'dev': 'xadmin/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js',
+        }
+    },
+    "flot": {
+        "js": {
+            'dev': 'xadmin/vendor/flot/js/jquery.flot.js',
+        }
+    },
+    "image-gallery": {
+        "css": {
+            'dev': 'xadmin/vendor/bootstrap-image-gallery/css/bootstrap-image-gallery.css',
+            'production': 'xadmin/vendor/bootstrap-image-gallery/css/bootstrap-image-gallery.min.css',
+        },
+        "js": {
+            'dev': ['xadmin/vendor/load-image/load-image.js', 'xadmin/vendor/bootstrap-image-gallery/js/bootstrap-image-gallery.js'],
+            'production': ['xadmin/vendor/load-image/load-image.min.js', 'xadmin/vendor/bootstrap-image-gallery/js/bootstrap-image-gallery.min.js']
+        }
+    },
+    "select2": {
+        "css": {
+            'dev': 'xadmin/vendor/select2/select2.css',
+        },
+        "js": {
+            'dev': 'xadmin/vendor/select2/select2.js',
+            'production': 'xadmin/vendor/select2/select2.min.js',
+        }
+    },
 }
