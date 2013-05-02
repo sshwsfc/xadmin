fc18dfadec1555087d83ea171430c5866c9c8ff5
sshwsfc <sshwsfc@gmail.com>
2013-05-02 12:44:55 +0800

    Add login and logout views filter hook

--- xadmin/views/website.py
+++ xadmin/views/website.py
@@ -5,14 +5,15 @@ from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from django.http import HttpResponse
 
-from base import BaseAdminView
+from base import BaseAdminView, filter_hook
 from dashboard import Dashboard
 from xadmin.forms import AdminAuthenticationForm
 from xadmin.models import UserSettings
+from xadmin.layout import FormHelper
 
 
 class IndexView(Dashboard):
-    title = _(u"Main Dashboard")
+    title = _("Main Dashboard")
     icon = "dashboard"
 
 class UserSettingView(BaseAdminView):
@@ -28,20 +29,32 @@ class UserSettingView(BaseAdminView):
 
 class LoginView(BaseAdminView):
 
+    title = _("Please Login")
+    login_form = None
+    login_template = None
+
+    @filter_hook
+    def update_params(self, defaults):
+        pass
+
     @never_cache
     def get(self, request, *args, **kwargs):
         context = self.get_context()
+        helper = FormHelper()
+        helper.form_tag = False
         context.update({
-            'title': _('Log in'),
+            'title': self.title,
+            'helper': helper,
             'app_path': request.get_full_path(),
             REDIRECT_FIELD_NAME: request.get_full_path(),
         })
         defaults = {
             'extra_context': context,
             'current_app': self.admin_site.name,
-            'authentication_form': AdminAuthenticationForm,
-            'template_name': 'xadmin/views/login.html',
+            'authentication_form': self.login_form or AdminAuthenticationForm,
+            'template_name': self.login_template or 'xadmin/views/login.html',
         }
+        self.update_params(defaults)
         return login(request, **defaults)
 
     @never_cache
@@ -52,6 +65,10 @@ class LogoutView(BaseAdminView):
 
     logout_template = None
 
+    @filter_hook
+    def update_params(self, defaults):
+        pass
+        
     @never_cache
     def get(self, request, *args, **kwargs):
         context = self.get_context()
@@ -62,6 +79,7 @@ class LogoutView(BaseAdminView):
         if self.logout_template is not None:
             defaults['template_name'] = self.logout_template
 
+        self.update_params(defaults)
         return logout(request, **defaults)
 
     @never_cache
