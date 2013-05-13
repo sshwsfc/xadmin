f63c442badc69be26c419f6a2982ff12a0f715ec
sshwsfc <sshwsfc@gmail.com>
2013-05-13 16:13:33 +0800

    Update version to 0.1.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-xadmin',
-    version='0.1.10-dev',
+    version='0.1.12-dev',
     description='Drop-in replacement of Django admin comes with lots of goodies, fully extensible with plugin support, pretty UI based on Twitter Bootstrap.',
     long_description=open('README.md').read(),
     author='sshwsfc',
--- xadmin/__init__.py
+++ xadmin/__init__.py
@@ -1,6 +1,6 @@
 from xadmin.sites import AdminSite, site
 
-VERSION = [0, 1, 0]
+VERSION = [0, 1, 12]
 
 
 def autodiscover():
