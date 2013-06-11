d4e99deb7a2f7f07000d683592d75aacd34648dd
Simanas Venčkauskas <simanas.venckauskas@gmail.com>
2013-06-11 16:22:42 +0300

    django.utils.simplejson is depreciated

--- xadmin/models.py
+++ xadmin/models.py
@@ -1,3 +1,4 @@
+import json
 import django
 from django.db import models
 from django.conf import settings
@@ -10,8 +11,6 @@ from django.utils.encoding import smart_unicode
 
 from django.db.models.signals import post_syncdb
 from django.contrib.auth.models import Permission
-
-from util import json
 
 import datetime
 import decimal
