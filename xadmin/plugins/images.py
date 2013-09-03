c170d5942a2aaac423f7aa460884f3e1ae5706cb
sshwsfc <sshwsfc@gmail.com>
2013-09-03 17:58:47 +0800

    Active Image plugin.

--- xadmin/plugins/images.py
+++ xadmin/plugins/images.py
@@ -23,7 +23,7 @@ class AdminImageWidget(forms.FileInput):
         output = []
         if value and hasattr(value, "url"):
             label = self.attrs.get('label', name)
-            output.append('<a href="%s" target="_blank" title="%s" rel="gallery"><img src="%s" class="field_img"/></a><br/>%s ' %
+            output.append('<a href="%s" target="_blank" title="%s" data-gallery="gallery"><img src="%s" class="field_img"/></a><br/>%s ' %
                          (value.url, label, value.url, _('Change:')))
         output.append(super(AdminImageWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
@@ -46,7 +46,7 @@ class ModelDetailPlugin(BaseAdminPlugin):
         if isinstance(result.field, models.ImageField):
             if result.value:
                 img = getattr(result.obj, field_name)
-                result.text = mark_safe('<a href="%s" target="_blank" title="%s" rel="gallery"><img src="%s" class="field_img"/></a>' % (img.url, result.label, img.url))
+                result.text = mark_safe('<a href="%s" target="_blank" title="%s" data-gallery="gallery"><img src="%s" class="field_img"/></a>' % (img.url, result.label, img.url))
                 self.include_image = True
         return result
 
@@ -59,27 +59,34 @@ class ModelDetailPlugin(BaseAdminPlugin):
 
     def block_before_fieldsets(self, context, node):
         if self.include_image:
-            return """
-              <div id="gallery" data-toggle="modal-gallery" data-target="#modal-gallery">
-                <!-- modal-gallery is the modal dialog used for the image gallery -->
-                <div id="modal-gallery" class="modal modal-gallery hide fade" tabindex="-1">
-                    <div class="modal-header">
-                        <a class="close" data-dismiss="modal">&times;</a>
-                        <h3 class="modal-title"></h3>
-                    </div>
-                    <div class="modal-body"><div class="modal-image"></div></div>
-                    <div class="modal-footer">
-                        <a class="btn btn-primary modal-next">%s <i class="icon-arrow-right icon-white"></i></a>
-                        <a class="btn btn-info modal-prev"><i class="icon-arrow-left icon-white"></i> %s</a>
-                        <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000"><i class="icon-play icon-white"></i> %s</a>
-                        <a class="btn btn-default modal-download" target="_blank"><i class="icon-download"></i> %s</a>
-                    </div>
-                </div>
-            """ % (_('Next'), _('Previous'), _('Slideshow'), _('Download'))
+            return '<div id="gallery" data-toggle="modal-gallery" data-target="#modal-gallery">'
 
     def block_after_fieldsets(self, context, node):
         if self.include_image:
             return "</div>"
+
+    def block_extrabody(self, context, node):
+        if self.include_image:
+            return """
+                <!-- modal-gallery is the modal dialog used for the image gallery -->
+                <div id="modal-gallery" class="modal modal-gallery fade" tabindex="-1">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <a class="close" data-dismiss="modal">&times;</a>
+                        <h3 class="modal-title"></h3>
+                      </div>
+                      <div class="modal-body"><div class="modal-image"></div></div>
+                      <div class="modal-footer">
+                          <a class="btn btn-primary modal-next">%s <i class="icon-arrow-right icon-white"></i></a>
+                          <a class="btn btn-info modal-prev"><i class="icon-arrow-left icon-white"></i> %s</a>
+                          <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000"><i class="icon-play icon-white"></i> %s</a>
+                          <a class="btn btn-default modal-download" target="_blank"><i class="icon-download"></i> %s</a>
+                      </div>
+                    </div><!-- /.modal-content -->
+                  </div><!-- /.modal-dialog -->
+                </div>
+            """ % (_('Next'), _('Previous'), _('Slideshow'), _('Download'))
 
 site.register_plugin(ModelDetailPlugin, DetailAdminView)
 site.register_plugin(ModelDetailPlugin, ModelFormAdminView)
