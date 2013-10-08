5764c9e0d4127dc15f28ed228f083ca84854c02e
Areski Belaid <areski@gmail.com>
2013-10-09 00:58:59 +0200

    fix indent vendor.py

--- xadmin/vendors.py
+++ xadmin/vendors.py
@@ -11,10 +11,12 @@ vendors = {
             'production': 'xadmin/vendor/bootstrap/css/bootstrap.css',
             'cdn': 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css'
         },
-        'responsive': {'css':{
+        'responsive': {
+            'css': {
                 'dev': 'xadmin/vendor/bootstrap/bootstrap-responsive.css',
                 'production': 'xadmin/vendor/bootstrap/bootstrap-responsive.css'
-            }}
+            }
+        }
     },
     'jquery': {
         "js": {
