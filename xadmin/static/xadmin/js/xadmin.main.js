9a774cb095bcdf98f0383f81d9f0474c8c4bafe1
sshwsfc <sshwsfc@gmail.com>
2013-11-13 00:23:30 +0800

    Add nav-form block in base_site.html and remove buttons loading effect.

--- xadmin/static/xadmin/js/xadmin.main.js
+++ xadmin/static/xadmin/js/xadmin.main.js
@@ -59,11 +59,11 @@
   })
   
   // loading btn
-  $('.btn.btn-loading,.btn[type=submit]')
-    .click(function () {
-      var btn = $(this)
-      btn.button('loading')
-    })
+  // $('.btn.btn-loading,.btn[type=submit]')
+  //   .click(function () {
+  //     var btn = $(this)
+  //     btn.button('loading')
+  //   })
 
   //.nav-content bar nav-menu
   $('.navbar-xs .navbar-nav > li')
