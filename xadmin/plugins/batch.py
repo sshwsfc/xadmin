f3b2851b9f9be53349d6b896c53271a6e5813723
sshwsfc <sshwsfc@gmail.com>
2014-01-09 15:47:16 +0800

    Fix #90 _has_changed is Dropped in Django 1.6

--- xadmin/plugins/batch.py
+++ xadmin/plugins/batch.py
@@ -55,9 +55,6 @@ class ChangeFieldWidgetWrapper(forms.Widget):
     def value_from_datadict(self, data, files, name):
         return self.widget.value_from_datadict(data, files, name)
 
-    def _has_changed(self, initial, data):
-        return self.widget._has_changed(initial, data)
-
     def id_for_label(self, id_):
         return self.widget.id_for_label(id_)
 
