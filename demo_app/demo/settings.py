50e736c1a2d754b747544807651bfe9b50807e5a
sshwsfc <sshwsfc@gmail.com>
2016-02-03 09:49:47 +0800

    Add language switch demo in demo app.

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -45,7 +45,7 @@ LANGUAGE_CODE = 'en-us'
 
 LANGUAGES = (
     ('en', gettext('English')),
-    ('zh_CN', gettext('Chinese')),
+    ('zh-cn', gettext('Chinese')),
 )
 
 SITE_ID = 1
@@ -111,6 +111,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
