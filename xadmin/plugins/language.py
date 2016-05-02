dc76b718306e74fafcfb607312e950861fc5fc29
sshwsfc <sshwsfc@gmail.com>
2016-05-02 11:01:15 +0800

    Save user select language.

--- xadmin/plugins/language.py
+++ xadmin/plugins/language.py
@@ -1,9 +1,12 @@
 
 from django.conf import settings
 from django.template import loader, RequestContext
+from django.utils.translation import check_for_language
+from django.views.i18n import set_language
 
 from xadmin.sites import site
-from xadmin.views import BaseAdminPlugin, CommAdminView
+from xadmin.models import UserSettings
+from xadmin.views import BaseAdminPlugin, CommAdminView, BaseAdminView
 
 
 class SetLangNavPlugin(BaseAdminPlugin):
@@ -14,6 +17,21 @@ class SetLangNavPlugin(BaseAdminPlugin):
                 'redirect_to': self.request.get_full_path(),
             }, context_instance=RequestContext(self.request)))
 
+class SetLangView(BaseAdminView):
+
+    def post(self, request, *args, **kwargs):
+        response = set_language(request)
+
+        lang_code = request.POST.get('language', None)
+        if lang_code and check_for_language(lang_code):
+            us, created = UserSettings.objects.get_or_create(
+                user=self.user, key='i18n-language')
+            us.value = lang_code
+            us.save()
+
+        return response
+    
+
 if settings.LANGUAGES and 'django.middleware.locale.LocaleMiddleware' in settings.MIDDLEWARE_CLASSES:
     site.register_plugin(SetLangNavPlugin, CommAdminView)
-    site.register_view(r'^i18n/', lambda site: 'django.conf.urls.i18n', 'i18n')
+    site.register_view(r'^i18n_set_language', SetLangView, 'i18n_set_language')
