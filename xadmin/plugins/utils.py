0587be1c2622ef454306a445b4c3e56d3069be9e
Alex Sandro <geniofuturo@gmail.com>
2016-07-21 11:04:27 -0300

    Doc.

--- xadmin/plugins/utils.py
+++ xadmin/plugins/utils.py
@@ -2,6 +2,12 @@ from django.template.context import RequestContext
 
 
 def get_context_dict(context):
+    """
+     Contexts in django version 1.9+ must be dictionaries. As xadmin has a legacy with older versions of django,
+    the function helps the transition by converting the [RequestContext] object to the dictionary when necessary.
+    :param context: RequestContext
+    :return: dict
+    """
     if isinstance(context, RequestContext):
         ctx = {}
         map(ctx.update, context.dicts)
