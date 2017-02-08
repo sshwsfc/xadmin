b999fcee26305db295e75581de8852418edaa546
wgbbiao <wgb237@163.com>
2017-02-08 16:20:06 +0800

    删除encoding

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='xadmin',
     version='0.6.1',
     description='Drop-in replacement of Django admin comes with lots of goodies, fully extensible with plugin support, pretty UI based on Twitter Bootstrap.',
-    long_description=open('README.rst', encoding='utf-8').read(),
+    long_description=open('README.rst').read(),
     author='sshwsfc',
     author_email='sshwsfc@gmail.com',
     license=open('LICENSE').read(),
