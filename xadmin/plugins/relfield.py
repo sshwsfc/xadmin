c5792739acf2fe30916c5e74fe91b37335a7b849
wgbbiao <wgb237@163.com>
2017-05-22 15:25:14 +0800

    增加判断　extra_attrs　是否为空

--- xadmin/plugins/relfield.py
+++ xadmin/plugins/relfield.py
@@ -36,7 +36,8 @@ class ForeignKeySearchWidget(forms.Widget):
             attrs.update(kwargs)
             return super(ForeignKeySearchWidget, self).build_attrs(attrs, extra_attrs=extra_attrs)
         else:
-            attrs.update(extra_attrs)
+            if extra_attrs:
+                attrs.update(extra_attrs)
             return super(ForeignKeySearchWidget, self).build_attrs(attrs, **kwargs)
 
     def render(self, name, value, attrs=None):
