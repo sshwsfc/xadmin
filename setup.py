a89c17f4b5aba3f3cf6f384649475cbc240d2276
alex <geniofuturo@gmail.com>
2017-10-06 16:59:52 -0300

    Synchronizing dependencies (pip / setup).

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'setuptools',
         'django>=1.9.0,<2',
         'django-crispy-forms>=1.6.0',
+        'django-import-export>=0.5.1',
         'django-reversion>=2.0.0',
         'django-formtools>=1.0',
         'httplib2==0.9.2',
