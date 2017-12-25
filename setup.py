749ff5b7c60ab1094a7e93390bba335e78396e63
wgbbiao <wgb237@163.com>
2017-12-25 16:26:30 +0800

    删除手机插件

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='xadmin',
-    version='0.6.1',
+    version='2.0.1',
     description='Drop-in replacement of Django admin comes with lots of goodies, fully extensible with plugin support, pretty UI based on Twitter Bootstrap.',
     long_description=open('README.rst', encoding='utf-8').read(),
     author='sshwsfc',
