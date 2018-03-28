66db6d52d0303c1231d7cb954f04c9be4140a82b
xianbu <dang.xia@qq.com>
2018-03-28 10:23:51 +0800

    fix

--- xadmin/widgets.py
+++ xadmin/widgets.py
@@ -72,7 +72,7 @@ class AdminSplitDateTime(forms.SplitDateTimeWidget):
         forms.MultiWidget.__init__(self, widgets, attrs)
 
     def render(self, name, value, attrs=None):
-        input_html = [ht for ht in super(AdminSplitDateTime, self).render(name, value, attrs).split('\n') if ht != '']
+        input_html = [ht for ht in super(AdminSplitDateTime, self).render(name, value, attrs).split('>') if ht != '']
         # return input_html
         return mark_safe('<div class="datetime clearfix"><div class="input-group date bootstrap-datepicker"><span class="input-group-addon"><i class="fa fa-calendar"></i></span>%s'
                          '<span class="input-group-btn"><button class="btn btn-default" type="button">%s</button></span></div>'
