dadb99ab091870dabb7dbefa012276de3f810447
alex <alex@fabricadigital.com.br>
2018-05-22 18:03:31 -0300

    Removing unused code.

--- xadmin/views/edit.py
+++ xadmin/views/edit.py
@@ -183,12 +183,6 @@ class ModelFormAdminView(ModelAdminView):
 
         return modelform_factory(self.model, **defaults)
 
-        try:
-            return modelform_factory(self.model, **defaults)
-        except FieldError as e:
-            raise FieldError('%s. Check fields/fieldsets/exclude attributes of class %s.'
-                             % (e, self.__class__.__name__))
-
     @filter_hook
     def get_form_layout(self):
         layout = copy.deepcopy(self.form_layout)
