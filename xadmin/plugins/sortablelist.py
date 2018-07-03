f3dab3f8336c099b2d5f475ec14d294ce2ef7dc5
alexsilva <geniofuturo@gmail.com>
2018-07-03 12:32:54 -0300

    Add support to custom columns.

--- xadmin/plugins/sortablelist.py
+++ xadmin/plugins/sortablelist.py
@@ -22,6 +22,8 @@ from xadmin.views.base import csrf_protect_m
 class SortableListPlugin(BaseAdminPlugin):
 
     list_order_field = None
+    # Used in custom columns
+    list_order_display_field = None
 
     def init_request(self, *args, **kwargs):
         return bool(self.list_order_field)
@@ -38,7 +40,8 @@ class SortableListPlugin(BaseAdminPlugin):
         return row
 
     def result_item(self, item, obj, field_name, row):
-        if self.is_list_sortable and field_name == self.list_order_field:
+        if self.is_list_sortable and field_name in (self.list_order_field,
+                                                    self.list_order_display_field):
             item.btns.append('<a><i class="fa fa-arrows"></i></a>')
         return item
 
