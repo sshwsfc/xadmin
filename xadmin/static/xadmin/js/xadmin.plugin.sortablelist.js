d1c16b61e950a70d416a4432f5c2314e2042edf8
alexsilva <geniofuturo@gmail.com>
2019-05-21 16:59:09 -0300

    Changing the status of the button.

--- xadmin/static/xadmin/js/xadmin.plugin.sortablelist.js
+++ xadmin/static/xadmin/js/xadmin.plugin.sortablelist.js
@@ -32,6 +32,8 @@
             update: function(event, ui) {
                 var $rows = $(this);
                 $("#save-order").on("click", function(e) {
+                    self = $(this);
+                    self.addClass('disabled');
                     $.ajax({
                         url: $(this).attr('post-url'),
                         method: 'POST',
@@ -40,7 +42,10 @@
                             expression: (/(.+)_(.+)/),
                         })
                     }).done(function () {
+                        self.removeClass('disabled'); // for safety
                         location.reload(true);
+                    }).fail(function () {
+                        self.removeClass('disabled');
                     });
                 }).show();
             }
