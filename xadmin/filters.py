98cd246cc3f308e352a3c17ac275ca2f23d5ddb4
hzde0128 <hzde0128@live.cn>
2020-04-15 13:43:00 +0800

    resolve SyntaxWarning: "is" with a literal. Did you mean "=="?

--- xadmin/filters.py
+++ xadmin/filters.py
@@ -202,7 +202,7 @@ class ChoicesFieldListFilter(ListFieldFilter):
 
     def choices(self):
         yield {
-            'selected': self.lookup_exact_val is '',
+            'selected': self.lookup_exact_val == '',
             'query_string': self.query_string({}, [self.lookup_exact_name]),
             'display': _('All')
         }
@@ -548,7 +548,7 @@ class AllValuesFieldListFilter(ListFieldFilter):
 
     def choices(self):
         yield {
-            'selected': (self.lookup_exact_val is '' and self.lookup_isnull_val is ''),
+            'selected': (self.lookup_exact_val == '' and self.lookup_isnull_val == ''),
             'query_string': self.query_string({}, [self.lookup_exact_name, self.lookup_isnull_name]),
             'display': _('All'),
         }
