7ec5ac86a0c31bc0ef9b2cecc00256e8c32eb446
alex <geniofuturo@gmail.com>
2020-08-19 17:16:47 -0300

    Move 'setCookie' to xadmin-main.

--- xadmin/static/xadmin/js/xadmin.main.js
+++ xadmin/static/xadmin/js/xadmin.main.js
@@ -14,6 +14,29 @@
     $('.exform:not(.rended)').exform();
   });
 
+  $.setCookie = function(name, value, options){
+    options = options || {};
+    if (value === null) {
+        value = '';
+        options.expires = -1;
+    }
+    var expires = '';
+    if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+        var date;
+        if (typeof options.expires == 'number') {
+            date = new Date();
+            date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+        } else {
+            date = options.expires;
+        }
+        expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+    }
+    var path = options.path ? '; path=' + options.path : '';
+    var domain = options.domain ? '; domain=' + options.domain : '';
+    var secure = options.secure ? '; secure' : '';
+    document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+  }
+
   $.getCookie = function(name) {
       var cookieValue = null;
       if (document.cookie && document.cookie != '') {
