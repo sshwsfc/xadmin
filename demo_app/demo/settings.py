acc0d148459e812ec55f2567fb0dafbf2c1e0bea
alexsilva <geniofuturo@gmail.com>
2020-10-27 12:21:19 -0300

    Add context processors.

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -74,6 +74,10 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
