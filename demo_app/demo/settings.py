b9c9c9dd28adf222b778bf6e32199db647122609
alexsilva <geniofuturo@gmail.com>
2020-10-27 12:07:09 -0300

    Add LocaleMiddleware.

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'demo.urls'
