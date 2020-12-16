3bde9f5e220ec9f39680ea1450aefaff74daee63
alex <geniofuturo@gmail.com>
2020-12-16 17:25:01 -0300

    Convert Context to dict.

--- xadmin/plugins/utils.py
+++ xadmin/plugins/utils.py
@@ -1,14 +1,16 @@
-from django.template.context import RequestContext
+from django.template.context import RequestContext, Context
 
 
 def get_context_dict(context):
     """
      Contexts in django version 1.9+ must be dictionaries. As xadmin has a legacy with older versions of django,
-    the function helps the transition by converting the [RequestContext] object to the dictionary when necessary.
+    the function helps the transition by converting the [RequestContext, Context] object to the dictionary when necessary.
     :param context: RequestContext
     :return: dict
     """
     if isinstance(context, RequestContext):
+        ctx = context.flatten()
+    elif isinstance(context, Context):
         ctx = context.flatten()
     else:
         ctx = context
